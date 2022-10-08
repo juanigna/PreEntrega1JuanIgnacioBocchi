9afe641403e5c7c9f6aac42c9193c4edc29f0081
juanigna <jibocchi14@gmail.com>
2022-10-08 19:33:04 -0300

    Añado mas productos al ecommerce

--- src/components/mock/stock.js
+++ src/components/mock/stock.js
@@ -1,15 +1,11 @@
-import ekosPerfume from '../../assets/ekos-perfume.jpg';
-import cremaEkos from '../../assets/crema-natura.webp';
-import kitLimpieza from '../../assets/kit-limpieza.jpg';
-import desodorante from '../../assets/desodorante-roll.jpg';
-
 const stock = [
     {
         id: 1,
         name: 'Pulpa Manos Castaña',
         description: 'Crema Ekos Natura de 75g',
         price: 3000,
-        prod_img: cremaEkos,
+        prod_img:
+            'https://res.cloudinary.com/dzcujelju/image/upload/v1664896249/crema-natura_ybaaya.webp',
         category: 'cremas',
         stock: 5,
     },
@@ -18,7 +14,8 @@ const stock = [
         name: 'Ekos Alma Femenino',
         description: 'Perfuma de alta gama',
         price: 13000,
-        prod_img: ekosPerfume,
+        prod_img:
+            'https://res.cloudinary.com/dzcujelju/image/upload/v1664896249/ekos-perfume_q53bum.jpg',
         category: 'perfumes',
         stock: 8,
     },
@@ -27,7 +24,8 @@ const stock = [
         name: 'Kit Limpieza + tónico Chronos',
         description: 'Escencial para tu cuidado diario',
         price: 4060,
-        prod_img: kitLimpieza,
+        prod_img:
+            'https://res.cloudinary.com/dzcujelju/image/upload/v1664896249/kit-limpieza_fujzqo.jpg',
         category: 'limpieza',
         stock: 10,
     },
@@ -36,9 +34,30 @@ const stock = [
         name: 'Desodorante roll on aclarador',
         description: 'Desodorante en roll',
         price: 790,
-        prod_img: desodorante,
+        prod_img:
+            'https://res.cloudinary.com/dzcujelju/image/upload/v1664896249/desodorante-roll_fy1vya.jpg',
         category: 'limpieza',
         stock: 3,
+    },
+    {
+        id: 5,
+        name: 'Frescor Ekos Pitanga Preta',
+        description: 'Frescor Ekos Pitanga Preta 75ml',
+        price: 2.875,
+        prod_img:
+            'https://res.cloudinary.com/dzcujelju/image/upload/v1665267793/perfume-frescor_dspays.jpg',
+        category: 'perfumes',
+        stock: 12,
+    },
+    {
+        id: 6,
+        name: 'Hidratante de cuerpo Ekos Breu Branco ',
+        description: 'Hidratante de cuerpo Ekos Breu Branco 250ml',
+        price: 868,
+        prod_img:
+            'https://res.cloudinary.com/dzcujelju/image/upload/v1665268012/crema-2_h07eoy.jpg',
+        category: 'cremas',
+        stock: 7,
     },
 ];
 
